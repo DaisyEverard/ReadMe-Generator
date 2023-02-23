a2b9f7ae89cb9bd3ae087197a4f6fde3db20a9c9
DaisyEverard <msdaisyeverard@gmail.com>
2023-02-23 20:28:18 +0000

    FEAT: split answers into constants

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-
+  const {title, description, installation, usage, contributing, 
+    tests, license, questions} = data; 
+  
 }
 
 module.exports = generateMarkdown;
